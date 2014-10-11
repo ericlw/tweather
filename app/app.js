589c5fd791ea5a8813d937f177ac494fade2c322
noonan <noonan@twitter.com>
2014-10-11 15:12:12 -0700

    actually use zipcode and refactor, currently broken though

--- app/app.js
+++ app/app.js
@@ -23,41 +23,56 @@ stream.on('tweet', function (tweet) {
 });
 
 function handleTweet(tweet) {
+    var weather, status;
     var zip = extractCityOrZip(tweet.text);
     if (zip) {
-        console.log(zip[0])
+        getWeather(zip, function (data) {
+            if (data) {
+                weather = data;
+            }
+        });
     } else {
         var geo = tweet.coordinates;
         if (geo && geo.type == 'Point') {
             var lat = geo.coordinates[1];
             var lon = geo.coordinates[0];
-            request("http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + lat + "&lon=" + lon + "&cnt=1&mode=json&units=imperial",
-                function (err, response, body) {
-                    if (err) {
-                        return console.log("Error getting weather for lat=" + lat + " & lon=" + lon, err);
-                    }
-                    var weather = JSON.parse(body);
-
-                    var status = "@" + tweet.user.screen_name + " the forecast for " + weather.city.name +
-                        " today is " + weather.list[0].weather[0].main.toLowerCase() +
-                        " with a high of " + weather.list[0].temp.max + " and a low of " + weather.list[0].temp.min;
-
-                    twitter.post('statuses/update', { status: status, in_reply_to_status_id: tweet.id }, function (err, data, response) {
-                        if (err) {
-                            console.log(err);
-                        } else {
-                            console.log(data);
-                        }
-                    });
-                });
+            var query = "lat=" + lat + "&lon=" + lon;
+            getWeather(query, function (data) {
+                if (data) {
+                    weather = data;
+                }
+            });
         }
     }
+    if (weather) {
+        status = "@" + tweet.user.screen_name + " the forecast for " + weather.name +
+            " today is " + weather.weather[0].main.toLowerCase() +
+            " with a high of " + weather.main.temp_max + " and a low of " + weather.main.temp_min;
+    } else {
+        status = "@" + tweet.user.screen_name + " I cannot seem to find where you want the weather for. " +
+        "Please include a zip code or geo-tag your tweet!";
+    }
+    twitter.post('statuses/update', { status: status, in_reply_to_status_id: tweet.id_str }, function (err, data, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+function getWeather(query, callback) {
+    request("http://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + query, function (err, response, body) {
+        if (err) {
+            return console.log("Error getting weather for lat=" + lat + " & lon=" + lon, err);
+        }
+        var weather = JSON.parse(body);
+        callback(weather);
+    });
 }
 
 function extractCityOrZip(str) {
-    //the regular expression below is for 5 digit US ZIP code, 5 digit US ZIP code + 4,
-    //and 6 digit alphanumeric Canadian Postal Code
-    var re = /\d{5}-\d{4}|\d{5}|[A-Z]\d[A-Z] \d[A-Z]\d/
+    var re = /\d{5}/g;
 
-    return re.exec(str);
+    return  re.exec(str);
 }
